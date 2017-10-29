48765130c42caf3c4fc8297180724a7ac81ae2c9
Yuantao Xie <yuantaoxie1988@gmail.com>
2017-10-28 20:02:12 -0400

    Update pca.m

--- pca.m
+++ pca.m
@@ -1,6 +1,6 @@
 function [U, S] = pca(X)
 %PCA Run principal component analysis on the dataset X
-%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
+%   [U, S] = pca(X) computes eigenvectors of the covariance matrix of X
 %   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
 %
 
